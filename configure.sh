942b44d897c72f68d897c6e8ddae0f729863de49
Hady Mashhour <hady.mashhour@gmail.com>
2024-01-02 15:53:19 +0400

    added a line to delete google chrome if already installed to reinstall

--- configure.sh
+++ configure.sh
@@ -113,6 +113,8 @@ sudo apt-get install -y docker-ce
 echo "#############################"
 echo "Installing Google Chrome..."
 echo "#############################"
+# delete any files called google-chrome-stable_current_amd64.deb*
+sudo rm -f google-chrome-stable_current_amd64.deb*
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -f
