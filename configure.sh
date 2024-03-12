fb502f8ad547bd14d464b3564b1817ec6d1a9a70
Hady Mashhour <hady.mashhour@gmail.com>
2024-03-13 01:45:50 +0400

    added some more -ys

--- configure.sh
+++ configure.sh
@@ -69,8 +69,8 @@ currentdir=$PWD
 echo "#############################"
 echo "Updating apt..."
 echo "#############################"
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 
 # Ensure wget is installed
 if ! command -v wget &>/dev/null; then
@@ -126,7 +126,7 @@ echo "#############################"
 echo "Installing Node.js..."
 echo "#############################"
 # 1. Download and import the Nodesource GPG key
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
 sudo rm -f /etc/apt/keyrings/nodesource.gpg # Add this line
@@ -135,7 +135,7 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 # 3. Run Update and Install
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install nodejs -y
 
 echo "#############################"
